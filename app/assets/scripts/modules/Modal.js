162399be2f949049dc89ef1c980ea5a11596e431
Dyana Monroy <dyana.monroy17@live.com>
2019-11-23 17:34:00 -0500

    added a contact form

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -37,17 +37,21 @@ class Modal{
         document.body.insertAdjacentHTML('beforeend', `
         <div class="modal">
     <div class="modal__inner">
-      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Contact Me <strong>Here</strong></h2>
       <div class="wrapper wrapper--narrow">
-        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
-      </div>
+        <p class="modal__description">Hello, you may submit an message here. </p>
+      
+        <div class="contact-form">
+            <form id="contact-form" method="post" action="">
+                <input name="name" type="text" class="contact-form__inputs" placeholder ="Your Name" required><br>
+                <input name="email" type="email" class="contact-form__inputs" placeholder ="Your Email" required><br>
 
-      <div class="social-icons">
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
-      </div>
+                <textarea name="message" class="contact-form__inputs" placeholder="Message" rows="4" required></textarea><br>
+
+                <input type="submit" class="contact-form__inputs-submit" value="SEND MESSAGE">
+            </form>
+        </div>
+        </div>
     </div>
     <div class="modal__close">X</div>
   </div>
